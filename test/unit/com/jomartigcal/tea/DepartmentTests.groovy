d00b999c8d8b205e0b3c029630d50d4cf3a49d95
Jomar Tigcal <jomartigcal@gmail.com>
2010-07-23 01:18:15 +0800

    Updated the unit tests of Department and Teacher domain classes.

--- test/unit/com/jomartigcal/tea/DepartmentTests.groovy
+++ test/unit/com/jomartigcal/tea/DepartmentTests.groovy
@@ -1,18 +1,60 @@
 package com.jomartigcal.tea
 
+import org.codehaus.groovy.grails.plugins.GrailsPluginManager 
+import org.codehaus.groovy.grails.plugins.PluginManagerHolder 
 
 import grails.test.*
 
 class DepartmentTests extends GrailsUnitTestCase {
     protected void setUp() {
         super.setUp()
+		PluginManagerHolder.pluginManager = [hasGrailsPlugin: { String name -> true}] as GrailsPluginManager
     }
 
     protected void tearDown() {
         super.tearDown()
+		PluginManagerHolder.pluginManager = null
     }
 
-    void testSomething() {
-
+	void testConstraintsBlank() {
+		mockForConstraintsTests(Department)
+		
+		def department = new Department(code:"", name:"")
+		assertFalse "Validation should fail", department.validate()
+		assertTrue "Validation should have errors", department.hasErrors()
+		assertEquals "Code should not be blank", "blank", department.errors["code"]
+		assertEquals "Code should not be blank", "blank", department.errors["name"]
+	}
+	
+	void testConstraintsNull() {
+		mockForConstraintsTests(Department)
+		
+		def department = new Department()
+		assertFalse "Validation should fail", department.validate()
+		assertTrue "Validation should have errors", department.hasErrors()
+		assertEquals "Code should not be null", "nullable", department.errors["code"]
+		assertEquals "Code should not be null", "nullable", department.errors["name"]
+	}
+	
+	void testConstraintsUnique() {
+		def existingDept = new Department(code:"ITD", name:"IT Department")
+		mockDomain(Department, [existingDept])
+		
+		def department = new Department(code:"ITD", name:"IT Department")
+		assertFalse "Validation should fail", department.validate()
+		assertTrue "Validation should have errors", department.hasErrors()
+		assertEquals "Code should be unique", "unique", department.errors["code"]
+	}
+	
+	void testConstraintsValid() {
+		mockForConstraintsTests(Department)
+		
+		def department = new Department(code:"ITD", name:"IT Department")
+		assertTrue "Validation should not fail", department.validate()
+	}
+	
+    void testToString() {
+		def department = new Department(code:"ITD", name:"IT Department")
+		assertToString department, "ITD"
     }
 }
--- test/unit/com/jomartigcal/tea/TeacherTests.groovy
+++ test/unit/com/jomartigcal/tea/TeacherTests.groovy
@@ -1,18 +1,54 @@
 package com.jomartigcal.tea
 
+import org.codehaus.groovy.grails.plugins.GrailsPluginManager;
+import org.codehaus.groovy.grails.plugins.PluginManagerHolder;
 
 import grails.test.*
 
 class TeacherTests extends GrailsUnitTestCase {
     protected void setUp() {
         super.setUp()
+		PluginManagerHolder.pluginManager = [hasGrailsPlugin: { String name -> true}] as GrailsPluginManager
     }
 
     protected void tearDown() {
         super.tearDown()
+		PluginManagerHolder.pluginManager = null
     }
 
-    void testSomething() {
-
+    void testConstraintsBlank() {
+		mockForConstraintsTests(Teacher)
+		
+		def teacher = new Teacher(firstName: "", lastName:"")
+		assertFalse "Teacher instance should not pass validation", teacher.validate()
+		assertTrue "Teacher instance should have errors", teacher.hasErrors()
+		assertEquals "First name cannot be blank", "blank", teacher.errors["firstName"]
+		assertEquals "Last name cannot be blank", "blank", teacher.errors["lastName"];
     }
+	
+	void testConstraintsNull() {
+		mockForConstraintsTests(Teacher)
+		
+		def teacher = new Teacher()
+		assertFalse "Teacher instance should not pass validation", teacher.validate()
+		assertTrue "Teacher instance should have errors", teacher.hasErrors()
+		assertEquals "First name cannot be null", "nullable", teacher.errors["firstName"]
+		assertEquals "Last name cannot be null", "nullable", teacher.errors["lastName"];
+	}
+	
+	void testConstraintsValid() {
+		mockForConstraintsTests(Teacher)
+		
+		def teacher = new Teacher(firstName: "Jomar", middleInitial: "C", lastName:"Tigcal")
+		assertTrue "Teacher instance should pass validation", teacher.validate()
+	}
+	
+	void testToString(){
+		mockDomain(Teacher)
+		
+		def teacher = new Teacher(firstName: "Jomar", lastName:"Tigcal")
+		assertToString teacher, "Tigcal, Jomar"
+		teacher["middleInitial"] = "C"
+		assertToString teacher, "Tigcal, Jomar C."
+	}
 }
