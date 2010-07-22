c0d796ab63e5d4fce004e90e9f295f4e11b905a2
Jomar Tigcal <jomartigcal@gmail.com>
2010-07-23 01:17:24 +0800

    Added fields and constraints in Department and Teacher domain classes.

--- grails-app/domain/com/jomartigcal/tea/Department.groovy
+++ grails-app/domain/com/jomartigcal/tea/Department.groovy
@@ -2,6 +2,18 @@ package com.jomartigcal.tea
 
 class Department {
 
+	String code
+	String name
+	
+	static hasMany = [teachers: Teacher]
+	
     static constraints = {
+		code(blank:false, nullable:false, unique:true)
+		name(blank:false, nullable:false)
+		teachers(nullable:true)
     }
+	
+	String toString(){
+		code
+	}
 }
--- grails-app/domain/com/jomartigcal/tea/Teacher.groovy
+++ grails-app/domain/com/jomartigcal/tea/Teacher.groovy
@@ -2,6 +2,20 @@ package com.jomartigcal.tea
 
 class Teacher {
 
+	String firstName
+	String middleInitial
+	String lastName
+	
+	static belongsTo = [department:Department]
+	
     static constraints = {
+		firstName(blank:false, nullable:false)
+		middleInitial(range:'A'..'Z')
+		lastName(blank:false, nullable:false)
+		department(nullable:true)
     }
+	
+	String toString(){
+		"${lastName}, ${firstName}" + (middleInitial ? " ${middleInitial}." : "")
+	}
 }
